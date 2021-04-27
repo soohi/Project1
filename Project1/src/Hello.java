ffe60aedd37b3abcb04ca95f7aa357199857059c
EXEM <EXEM@DESKTOP-R0J7CS3>
2021-04-27 19:23:13 +0900

    1

---
+++ Project1/src/Hello.java
@@ -0,0 +1,18 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1000);
+		
+	}
+
+}
+
+		//1. Github 로그인
+		//2. 레파지토리(repository) 생성
+		//3. 프로젝트 및 클래스 생성
+		//4. 프로젝트 이름에서 우클릭-Team-Share Project
+		//5. 지역 레퍼지토리 설정
+		//6. 윈도우-show view-others-git staging
+		//뭐가 잇는디..
